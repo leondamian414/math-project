1c1964b1d0fb4ca82279bf74a390e99283dbc461
leondamian414 <leondamian414@gmail.com>
2025-03-13 13:18:20 +0000

    Add 8.ts

---
+++ src/8.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-math-project',
+  templateUrl: './math-project.component.html',
+  styleUrls: ['./math-project.component.css']
+})
+export class MathProjectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
